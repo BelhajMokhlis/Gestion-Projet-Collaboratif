7d7f1da997312d43f1a5f6ac46d03ed196364d3a
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-10-02 11:30:50 +0100

    GDPC-19 Repository Project

--- Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/repository/ProjectRepository.java
+++ Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/repository/ProjectRepository.java
@@ -1,5 +1,14 @@
 package repository;
 
-public class ProjectRepository {
+import java.util.List;
 
+import model.Project;
+
+public interface ProjectRepository {
+
+	void create(Project project);
+    void update(Project project);
+    void delete(int id);
+    Project findById(int id);
+    List<Project> findAll();
 }
