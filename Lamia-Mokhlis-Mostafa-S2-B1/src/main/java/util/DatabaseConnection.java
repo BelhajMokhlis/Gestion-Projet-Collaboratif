14ba227be70fa9280581d6e109e19aab54b42460
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-10-02 11:25:00 +0100

    class data base

--- Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/util/DatabaseConnection.java
+++ Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/util/DatabaseConnection.java
@@ -1,49 +1,69 @@
 package util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
 
 	private static DatabaseConnection instance;
-    private Connection connection;
-    private final String url = "jdbc:mysql://localhost:3306/projectmanagement";
-    private final String username = "root";
-    private final String password = "";
+	private Connection connection;
+	private String url;
+	private String username;
+	private String password;
 
-    private DatabaseConnection()  {
-        try {
-        	Class.forName("com.mysql.cj.jdbc.Driver"); 
-            this.connection = DriverManager.getConnection(url, username, password);
-        } catch (ClassNotFoundException e) {
-            System.err.println("MySQL JDBC Driver not found.");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.err.println("Erreur de connexion à la base de données. " + e.getMessage());
-        }
-    }
+	private DatabaseConnection() {
+		loadConfig();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			this.connection = DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException e) {
+			System.err.println("MySQL JDBC Driver not found.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.err.println("Erreur de connexion à la base de données. " + e.getMessage());
+		}
+	}
 
-    public Connection getConnection() {
-        return connection;
-    }
+	private void loadConfig() {
+		Properties prop = new Properties();
+		try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+			if (input == null) {
+				System.out.println("Sorry, unable to find config.properties");
+				return;
+			}
+			prop.load(input);
+			url = prop.getProperty("db.url");
+			username = prop.getProperty("db.username");
+			password = prop.getProperty("db.password");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    public static DatabaseConnection getInstance()  {
-        if (instance == null ) {
-            instance = new DatabaseConnection();
-        }
-        return instance;
-    }
-    
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public static DatabaseConnection getInstance()  {
+		if (instance == null ) {
+			instance = new DatabaseConnection();
+		}
+		return instance;
+	}
+	
  
-    public void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-                System.out.println("Connection closed.");
-            } catch (SQLException e) {
-                System.err.println("Error in closing connection. " + e.getMessage());
-            }
-        }
-    }
+	public void closeConnection() {
+		if (connection != null) {
+			try {
+				connection.close();
+				System.out.println("Connection closed.");
+			} catch (SQLException e) {
+				System.err.println("Error in closing connection. " + e.getMessage());
+			}
+		}
+	}
 }
