69b74f138b967e4d66ba3d818b591b119bd9982e
MesVortex <meskinemostafa4@gmail.com>
2024-10-02 15:19:45 +0100

    Update the TaskService class

--- Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/service/TaskService.java
+++ Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/service/TaskService.java
@@ -1,5 +1,31 @@
 package service;
 
-public class TaskService {
+import model.Task;
+import repository.Interface.TaskRepository;
+import repository.impl.TaskRepositoryImpl;
 
+import java.util.List;
+
+public class TaskService {
+    private final TaskRepository taskRepository = new TaskRepositoryImpl();
+
+    public void createTask(Task task) {
+        taskRepository.create(task);
+    }
+
+    public Task getTask(int taskID) {
+        return taskRepository.read(taskID);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.getAll();
+    }
+
+    public void updateTask(Task task) {
+        taskRepository.update(task);
+    }
+
+    public void deleteTask(int taskID) {
+        taskRepository.delete(taskID);
+    }
 }
