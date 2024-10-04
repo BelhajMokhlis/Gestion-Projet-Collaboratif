f1a3c446688178e748dd1094bbefdc2e06ea378f
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-10-04 12:35:16 +0100

    Your commit message describing the changes

--- Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/repository/interface/TeamRepository.java
+++ Lamia-Mokhlis-Mostafa-S2-B1/src/main/java/repository/interface/TeamRepository.java
@@ -1,14 +1,5 @@
 package repository.Interface;
 
-import java.util.List;
-
-import model.Team;
-
 public interface TeamRepository {
-    boolean addTeam(Team team);
-    boolean removeTeam(Team team);
-    boolean updateTeam(Team team);
-    Team getTeam(int id);
-    List<Team> getAllTeams();
 
 }
